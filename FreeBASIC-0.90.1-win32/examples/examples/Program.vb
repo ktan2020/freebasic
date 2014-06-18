729ff324abe47efdccf60a8b286bf994d934672b
demo <demo@demo>
2014-06-17 21:35:42 -0700

    First commit.

---
+++ FreeBASIC-0.90.1-win32/examples/examples/Program.vb
@@ -0,0 +1,18 @@
+﻿'
+' Created by SharpDevelop.
+' User: ktan1
+' Date: 1/26/2014
+' Time: 4:20 PM
+' 
+' To change this template use Tools | Options | Coding | Edit Standard Headers.
+'
+Module Program
+	Sub Main()
+		Console.WriteLine("Hello World!")
+		
+		' TODO: Implement Functionality Here
+		
+		Console.Write("Press any key to continue . . . ")
+		Console.ReadKey(True)
+	End Sub
+End Module
